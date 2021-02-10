9644943fc0007e39f56a20e92ca3a4dde2e00515
HiuTrun <62102580+HiuTrun@users.noreply.github.com>
2021-02-10 22:33:50 +0700

    update navigation bottom bar feature and layout XML

---
+++ app/src/main/java/hiutrun/example/newsapp/fragment/ArticleFragment.kt
@@ -0,0 +1,10 @@
+package hiutrun.example.newsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import hiutrun.example.newsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+
+}
---
+++ app/src/main/java/hiutrun/example/newsapp/fragment/SearchNewsFragment.kt
@@ -0,0 +1,9 @@
+package hiutrun.example.newsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import hiutrun.example.newsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+}
