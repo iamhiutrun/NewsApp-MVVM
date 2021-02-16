592578e7a7b2f5a5067a51cb41e80c6aa91f0dfe
HiuTrun <62102580+HiuTrun@users.noreply.github.com>
2021-02-16 13:09:36 +0700

    Search feature actived

--- app/src/main/java/hiutrun/example/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/hiutrun/example/newsapp/repository/NewsRepository.kt
@@ -8,4 +8,7 @@ class NewsRepository(
 ) {
     suspend fun getBreakingNews(country:String,pageNumber:Int) =
             RetrofitInstance.api.getBreakingNews(country,pageNumber)
+
+    suspend fun searchNews(searchQuery: String,pageNumber: Int) =
+            RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
 }
--- app/src/main/java/hiutrun/example/newsapp/utils/Constants.kt
+++ app/src/main/java/hiutrun/example/newsapp/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "8cd6b347805c422287f22812f9987f34"
         const val BASE_URL = "http://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
